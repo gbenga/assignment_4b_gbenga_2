bd701f6e091c1694e621026d0bb89ab0b7d40b78
Gbenga Ojo-Aromokudu <57733903+gbenga@users.noreply.github.com>
2024-11-12 13:49:18 -0600

    More hangman

--- problem_8/game.py
+++ problem_8/game.py
@@ -25,27 +25,31 @@ random_word = choose_word()
 print(random_word)
 
 def take_user_input():
-  global guess
-  letter = input("Guess a letter? ")
-  guess_letter(letter)
+  global guesses_remaining
+  if guesses_remaining >= 1:
+    letter = input("Guess a letter? ")
+    if letter.isalpha():
+      guess_letter(letter)
+    else:
+      print("you can only guess letters")
+      take_user_input()
 
 
 def wrong_letter(letter):
-  global guesses_remaining
+  # global guesses_remaining
   print(f"{letter} is not in the word.")
-  print(f"You have {guesses_remaining} guesses remaining.")
+  # print(f"You have {guesses_remaining} guesses remaining.")
   take_user_input()
 
 def update_part_word(letter):
   global part_word
 
   part_word_list = list(part_word)
-  print(part_word_list)
   for idx, r in enumerate(random_word):
     if r == letter:
       part_word_list[idx] = letter.upper()
   part_word = "".join(part_word_list)
-  print(part_word)
+
   pass
 
 def correct_letter(letter):
@@ -62,11 +66,9 @@ def guess_word():
 def guess_letter(letter):
   global guesses_remaining
   guesses_remaining -= 1
-  if guesses_remaining <= 0:
+  if guesses_remaining == 0:
     print("you're out of guesses")
     print(f"the word was {random_word}")
-    return
-  #TODO stop the extra guesses remaining print statements
   if letter.lower() in random_word:
     correct_letter(letter)
     print(f"You have {guesses_remaining} guesses remaining")
@@ -82,4 +84,3 @@ def guess_letter(letter):
   else:
     wrong_letter(letter)
     print(f"You have {guesses_remaining} guesses remaining")
-
