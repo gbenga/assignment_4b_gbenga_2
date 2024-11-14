eeca611c6f53caee4dc07ca4795a039b9171718d
Gbenga Ojo-Aromokudu <57733903+gbenga@users.noreply.github.com>
2024-11-14 00:34:49 -0600

    File walk

--- problem_2.py
+++ problem_2.py
@@ -1,41 +1,22 @@
-import sys
 # os.walk()
+import sys
+indentation = ''
 def file_walk(input):
-  # folder_name = ""
-  # for idx, i in enumerate(input):
-  end_found = False
+  global indentation
+  input = indentation + input
   try:
-    # find index of first / print everything before it
     slash_index = input.index("/")
   except:
-    end_found = True
     print(input)
-    print("end of path")
   else:
-    pass
-  finally:
-    if not end_found:
-      # recurse
-    pass
-    # print(input[:slash_index])
-    # remainder = input[slash_index:]
-    # if len(remainder) <= 0:
-    #   return 
-    # else:
-    #   file_walk(remainder)
-    # chop that off and then put it back through the function
-    # if what you want to put through the function the second time is nothing then stop
-
-
-    # if i == "/":
-    #   print(i)
-    #   if idx == len(input):
-    #     return
-    #   else:
-    #     file_walk(input[idx:])
-  #   else:
-  #     folder_name += i
-  # print(folder_name + "/")
+    print(input[:slash_index + 1])
+    if "/" not in input:
+      print("base")
+      return
+    else:
+      indentation += ' '
+      file_walk(input[slash_index + 1:])
+      return
 
 script, first = sys.argv
 file_walk(first)
